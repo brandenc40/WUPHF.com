7e185a11a5d61ea294c7da4c9e72ff95ba86e510
Branden Colen <BrandenColen@brandens-mbp-2.lan>
2020-03-14 20:20:36 -0500

    included built react acpp

---
+++ wuphf-frontend/build/precache-manifest.e1f5a01f38bcf7c4cc555e03ab8730e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53ad94a40880514cdb2543feef9c079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e802f65b07804a45bf8b",
+    "url": "/static/css/main.1a78c02c.chunk.css"
+  },
+  {
+    "revision": "a4bf729151a8680db468",
+    "url": "/static/js/2.20cd40d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.20cd40d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e802f65b07804a45bf8b",
+    "url": "/static/js/main.7948a2d9.chunk.js"
+  },
+  {
+    "revision": "4a128487e027368da198",
+    "url": "/static/js/runtime-main.be8dcdc9.js"
+  },
+  {
+    "revision": "17bc4435cc71e5a03bf0633aa9089c3e",
+    "url": "/static/media/logo.17bc4435.png"
+  },
+  {
+    "revision": "255f0e1655f2e84e9e0fe91dd32d49a5",
+    "url": "/static/media/wuphf_wuphfalert.255f0e16.wav"
+  }
+]);
